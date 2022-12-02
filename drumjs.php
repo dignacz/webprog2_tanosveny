11f01fde0a37360feaee1e5b809359352cb2292d
skafula <79100340+skafula@users.noreply.github.com>
2022-12-02 02:40:27 +0100

    js

---
+++ drumjs.php
@@ -0,0 +1,30 @@
+
+
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+  
+</head>
+
+<body>
+<div class="container">
+  <div class="row">
+  <h1 id="title">Drum 🥁 Kit</h1>
+  <div class="set">
+    <button class="w drum">w</button>
+    <button class="a drum">a</button>
+    <button class="s drum">s</button>
+    <button class="d drum">d</button>
+    <button class="j drum">j</button>
+    <button class="k drum">k</button>
+    <button class="l drum">l</button>
+  </div>
+
+
+
+  <script type="text/javascript" src="js/index.js"></script>
+</div>
+</div>
+</body>
+
+</html>
